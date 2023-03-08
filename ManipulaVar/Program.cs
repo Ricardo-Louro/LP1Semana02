e45c5e301dca0d5137f047b47e8a7af44b221ef3
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-03-08 22:26:34 +0000

    Add commentary

--- ManipulaVar/Program.cs
+++ ManipulaVar/Program.cs
@@ -2,16 +2,30 @@
 
 namespace ManipulaVar
 {
+    /// <summary>
+    /// This program will convert the first argument provided into int, then
+    /// utilize it in an operation with ++ and another with --
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Receive the arguments provided through the console
+        /// </summary>
+        /// <param name="args">String able to be converted into an int</param>
         static void Main(string[] args)
         {
+            //Convert the argument into an int
             int x = int.Parse(args[0]);
+            //Increment it after evaluating x for the current expression 
             x++;
+            //Print the current value of x
             Console.WriteLine($"{x}");
         
+            //Convert the argument into an int
             x = int.Parse(args[0]);
+            //Decrement it before evaluating x for the current expression
             --x;
+            //Print the current value of x
             Console.WriteLine($"{x}");
         }
     }
