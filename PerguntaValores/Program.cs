4a093e3b0f370b422c0b770783d2bd7f23201e75
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-03-08 22:57:55 +0000

    Receive player inputs, convert to float and store them

---
+++ PerguntaValores/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PerguntaValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide the cylinder's height:");
+            float a = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Please provide the cylinder's radius:");
+            float r = Convert.ToSingle(Console.ReadLine());
+        }
+    }
+}
