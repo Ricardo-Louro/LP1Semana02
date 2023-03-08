4f7164ef638018bc1dec595198a331347839487b
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-03-08 23:06:07 +0000

    Calculate the volume and surface of the cylinder and display it

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -10,6 +10,12 @@ namespace PerguntaValores
             float a = Convert.ToSingle(Console.ReadLine());
             Console.WriteLine("Please provide the cylinder's radius:");
             float r = Convert.ToSingle(Console.ReadLine());
+
+            float pi = 3.1415926f;
+
+            Console.WriteLine();
+            Console.WriteLine($"Volume: {pi * (r*r) * a}");
+            Console.WriteLine($"Surface's Area: {2* pi * r * (r + a)}");
         }
     }
 }
