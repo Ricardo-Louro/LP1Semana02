200356a9533869b129a8c6df830650fb2f4a0c6f
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-03-10 13:44:20 +0000

    Add Triangle with relevant commentary

---
+++ Triangle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Triangle
+{
+    /// <summary>
+    /// Receive via command line a character and a number and create a triangle
+    /// using those parameters.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //For loop which increments a variable i each loop until the number
+            //provided via the command line
+            for(int i=1; i<=int.Parse(args[0]); i++)
+            {
+                //Creates a string with the character provided via the command
+                //repeated i times
+                string output = new String (char.Parse(args[1]), i);
+                
+                //Prints the intended string
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
