6f1047e42145b6b7eaa24dae537f38fca6b0727e
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-03-10 15:05:08 +0000

    Add CrossSquare

--- CrossSquare/Program.cs
+++ CrossSquare/Program.cs
@@ -6,7 +6,36 @@ namespace CrossSquare
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int number = int.Parse(args[0]);
+
+            for (int i = 1; i <= number; i++)
+            {
+                if(i == 1 || i == number)
+                {
+                    string cardinal_line = new String(char.Parse("#"), number);
+                    Console.WriteLine(cardinal_line);
+                }
+                else
+                {
+                    for (int x = 1; x <= number; x++)
+                    {
+                        if(x == 1 || x == number)
+                        {
+                            Console.Write("#");
+                        }
+                        else if(x == i || x == number - i + 1)
+                        {
+                            Console.Write("X");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                        
+                    }
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
