17c7bccbd35405f90e6d56d017535e5d7bceb06d
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-03-10 02:22:01 +0000

    Edit ChatGPTGrandfather to work via Switch instruction

---
+++ GranpaPatternSwitch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GranpaPatternSwitch
+{
+    /// <summary>
+    /// Receive questions and reply with a response based on the question
+    /// </summary>
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Prompt the player to provide his question
+            Console.WriteLine("Place your question? ");
+
+            //Receive and store the player's input
+            string question = Console.ReadLine();
+
+            //Begin the switch instruction to receive the question
+            // and output the response
+            string response = question switch
+            {
+                "How are you?" => "I'm fine, thank you",
+                "What's your name?" => "Gran'pa",
+                "What's your mission?" => "Destroy mankind!",
+                _ => "You got me, I'm not THAT smart!",
+            };
+
+            //Print the response
+            Console.WriteLine(response);
+        }
+    }
+}
